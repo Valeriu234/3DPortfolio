3da7e1f255ae374662664dc4204f0747cdde42b4
Valeriu <valeriu.cazacu@smartdata.solutions>
2023-03-14 17:38:17 +0200

    Finishing navbar, introducing 3d libraries and implementing a 3d computer in the hero page

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Html, useProgress } from "@react-three/drei";
 
 const Loader = () => {
-    return (
-        <div>
-            Loader
-        </div>
-    );
+  const { progress } = useProgress();
+  return (
+    <Html>
+      <span className="canvas-load"></span>
+      <p className="font-bold font-[14px] text-[#f1f1f1] mt-40">
+        {progress.toFixed()}%
+      </p>
+    </Html>
+  );
 };
 
 export default Loader;
---
+++ src/components/canvas/index.js
@@ -0,0 +1,11 @@
+import BallCanvas from './Ball'
+import EarthCanvas from './Earth'
+import ComputersCanvas from './Computers'
+import StarsCanvas from './Stars'
+
+export {
+    BallCanvas,
+    EarthCanvas,
+    ComputersCanvas,
+    StarsCanvas
+}
